a2b098d964e3c4c573436c2fbc6d7ed51e4da0ab
Enrique Mateo Segundo <kikemat1606@gmail.com>
2026-03-08 23:19:07 -0600

    actualizacion

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const botonNo = document.getElementById("no");
 const botonSi = document.getElementById("si");
 
-/* mover botón NO por toda la pantalla */
+/* mover botón NO */
 
-botonNo.addEventListener("mouseover", ()=>{
+botonNo.addEventListener("mouseover",()=>{
 
 const ancho = window.innerWidth - botonNo.offsetWidth;
 const alto = window.innerHeight - botonNo.offsetHeight;
@@ -16,27 +16,36 @@ botonNo.style.top = y + "px";
 
 });
 
+/* pantalla final */
 
-/* pantalla cuando presiona SI */
-
-botonSi.addEventListener("click", ()=>{
+botonSi.addEventListener("click",()=>{
 
 document.body.innerHTML = `
 
-<div style="height:100vh;display:flex;flex-direction:column;
-justify-content:center;align-items:center;
+<div style="
+height:100vh;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
 background:linear-gradient(135deg,#ff9a9e,#fad0c4);
-font-family:Segoe UI;text-align:center;">
+text-align:center;
+color:white;
+font-family:Segoe UI;
+">
 
-<h1 style="color:white;font-size:50px;margin-bottom:20px;">
-Sabía que dirías que sí ❤️
-</h1>
+<h1 style="font-size:50px;">Sabía que dirías que sí ❤️</h1>
 
-<img src="img/foto.jpg" 
-style="width:300px;border-radius:20px;
-box-shadow:0 10px 25px rgba(0,0,0,0.3);">
+<img 
+src="https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2"
+style="
+width:350px;
+border-radius:20px;
+margin-top:20px;
+box-shadow:0 15px 30px rgba(0,0,0,0.3);
+">
 
-<p style="font-size:25px;color:white;margin-top:20px;">
+<p style="font-size:25px;margin-top:20px;">
 Te quiero mucho 🥰
 </p>
 
