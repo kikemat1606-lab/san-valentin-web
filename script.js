3c46fe77918789de1503fc1743e0a5b639354fd7
Enrique Mateo Segundo <kikemat1606@gmail.com>
2026-03-08 23:47:57 -0600

    agregando imagen

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const botonNo = document.getElementById("no");
-const botonSi = document.getElementById("si");
-
-/* mover botón NO */
-
-botonNo.addEventListener("mouseover",()=>{
-
-const ancho = window.innerWidth - botonNo.offsetWidth;
-const alto = window.innerHeight - botonNo.offsetHeight;
-
-const x = Math.random() * ancho;
-const y = Math.random() * alto;
-
-botonNo.style.left = x + "px";
-botonNo.style.top = y + "px";
-
-});
-
-/* pantalla final */
-
 botonSi.addEventListener("click",()=>{
 
 document.body.innerHTML = `
@@ -37,7 +17,7 @@ font-family:Segoe UI;
 <h1 style="font-size:50px;">Sabía que dirías que sí ❤️</h1>
 
 <img 
-src="https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2"
+src="img/perrita.jpg"
 style="
 width:350px;
 border-radius:20px;
@@ -46,7 +26,7 @@ box-shadow:0 15px 30px rgba(0,0,0,0.3);
 ">
 
 <p style="font-size:25px;margin-top:20px;">
-Te quiero mucho 🥰
+ESO PERRITA 🥰
 </p>
 
 </div>
